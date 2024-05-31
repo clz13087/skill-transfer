97b1971f49734596e363a32a2869514470defaed
clz13087 <clz13087@nitech.jp>
2024-05-31 16:25:32 +0900

    fix

--- SkillTransfer/Robot/xArmTransform.py
+++ SkillTransfer/Robot/xArmTransform.py
@@ -49,21 +49,21 @@ class xArmTransform:
 
         relativepos_mm = relativepos * 1000
 
-        # if self.mount == "left":
-        #     x, y, z = relativepos_mm[2] + self.__initX, -1 * relativepos_mm[1] + self.__initY, relativepos_mm[0] + self.__initZ
-        #     roll, pitch, yaw = -1 * relativerot[1] + self.__initRoll, -1 * relativerot[2] + self.__initPitch, relativerot[0] + self.__initYaw
-
         if self.mount == "left":
             x, y, z = relativepos_mm[2] + self.__initX, -1 * relativepos_mm[1] + self.__initY, relativepos_mm[0] + self.__initZ
-            roll, pitch, yaw = relativerot[2] + self.__initRoll, -1 * relativerot[1] + self.__initPitch, relativerot[0] + self.__initYaw
+            roll, pitch, yaw = -1 * relativerot[1] + self.__initRoll, -1 * relativerot[2] + self.__initPitch, relativerot[0] + self.__initYaw
 
-        # elif self.mount == "right":
-        #     x, y, z = relativepos_mm[2] + self.__initX, relativepos_mm[1] + self.__initY, -1 * relativepos_mm[0] + self.__initZ
-        #     roll, pitch, yaw = -1 * relativerot[1] + self.__initRoll, relativerot[2] + self.__initPitch, -1 * relativerot[0] + self.__initYaw
+        # if self.mount == "left":
+        #     x, y, z = relativepos_mm[2] + self.__initX, -1 * relativepos_mm[1] + self.__initY, relativepos_mm[0] + self.__initZ
+        #     roll, pitch, yaw = relativerot[2] + self.__initRoll, -1 * relativerot[1] + self.__initPitch, relativerot[0] + self.__initYaw
 
         elif self.mount == "right":
             x, y, z = relativepos_mm[2] + self.__initX, relativepos_mm[1] + self.__initY, -1 * relativepos_mm[0] + self.__initZ
-            roll, pitch, yaw = relativerot[2] + self.__initRoll, relativerot[1] + self.__initPitch, -1 * relativerot[0] + self.__initYaw
+            roll, pitch, yaw = -1 * relativerot[1] + self.__initRoll, relativerot[2] + self.__initPitch, -1 * relativerot[0] + self.__initYaw
+
+        # elif self.mount == "right":
+        #     x, y, z = relativepos_mm[2] + self.__initX, relativepos_mm[1] + self.__initY, -1 * relativepos_mm[0] + self.__initZ
+        #     roll, pitch, yaw = relativerot[2] + self.__initRoll, relativerot[1] + self.__initPitch, -1 * relativerot[0] + self.__initYaw
 
         elif self.mount == "flat":
             x, y, z = relativepos_mm[2] + self.__initX, relativepos_mm[0] + self.__initY, relativepos_mm[1] + self.__initZ
