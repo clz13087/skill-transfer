842fcabf59a50a1908f876d2490bb4698f06beaf
clz13087 <clz13087@nitech.jp>
2024-09-27 11:25:26 +0900

    add timestamp

--- SkillTransfer/Recorder/DataRecord.py
+++ SkillTransfer/Recorder/DataRecord.py
@@ -3,6 +3,7 @@ import datetime
 import math
 import os
 import threading
+import time
 
 import numpy as np
 import tqdm
@@ -15,6 +16,7 @@ class DataRecordManager:
     dictWeightPosition = {}
     dictWeightRotation = {}
     dictGripperValue_P = {}
+    dictTime = []
     dictDurationTime = []
     dictRobotPosition = {}
     dictRobotRotation = {}
@@ -90,6 +92,8 @@ class DataRecordManager:
         bendingSensor: dict
             Bending sensor values
         """
+        current_time = time.time()
+        self.dictTime.append([current_time])
 
         self.dictDurationTime.append([duration])
 
@@ -116,29 +120,31 @@ class DataRecordManager:
         dirPath: (Optional) str
             Directory path (not include the file name).
         """
-        transformHeader = ["time", "x", "y", "z", "qx", "qy", "qz", "qw", "weightpos", "weightrot"]
-        robotHeader = ["time", "x", "y", "z", "qx", "qy", "qz", "qw"]
-        headHeader = ["time", "x", "y", "z", "rx", "ry", "rz"]
+        transformHeader = ["timestamp", "time", "x", "y", "z", "qx", "qy", "qz", "qw", "weightpos", "weightrot"]
+        robotHeader = ["timestamp", "time", "x", "y", "z", "qx", "qy", "qz", "qw"]
+        headHeader = ["timestamp", "time", "x", "y", "z", "rx", "ry", "rz"]
 
         print("\n---------- DataRecordManager.ExportSelf ----------")
         print("Writing: Participant transform...")
         for i in tqdm.tqdm(range(self.participantNum), ncols=150):
+            npTime = np.array(self.dictTime)
             npDuration = np.array(self.dictDurationTime)
             npPosition = np.array(self.dictPosition["participant" + str(i + 1)])
             npRotation = np.array(self.dictRotation["participant" + str(i + 1)])
             npWeightPosition = np.array(self.dictWeightPosition["participant" + str(i + 1)])
             npWeightRotation = np.array(self.dictWeightRotation["participant" + str(i + 1)])
             npParticipantTransform = np.concatenate([npPosition, npRotation], axis=1)
-            npTimeParticipantTransform = np.c_[npDuration, npParticipantTransform, npWeightPosition, npWeightRotation]
+            npTimeParticipantTransform = np.c_[npTime, npDuration, npParticipantTransform, npWeightPosition, npWeightRotation]
             self.ExportAsCSV(npTimeParticipantTransform, dirPath, "Transform_Participant_" + str(i + 1), participant, conditions, number, transformHeader)
 
         print("Writing: Other rigid body transform...")
         for i in tqdm.tqdm(range(self.otherRigidBodyNum), ncols=150):
+            npTime = np.array(self.dictTime)
             npDuration = np.array(self.dictDurationTime)
             npPosition = np.array(self.dictPosition["otherRigidBody" + str(i + 1)])
             npRotation = np.array(self.dictRotation["otherRigidBody" + str(i + 1)])
             npRigidBodyTransform = np.concatenate([npPosition, npRotation], axis=1)
-            npTimeRigidBodyTransform = np.c_[npDuration, npRigidBodyTransform]
+            npTimeRigidBodyTransform = np.c_[npTime, npDuration, npRigidBodyTransform]
             self.ExportAsCSV(npTimeRigidBodyTransform, dirPath, "OtherRigidBody_" + str(i + 1), participant, conditions, number, transformHeader)
 
         # print("Writing: Participant Gripper value...")
@@ -148,11 +154,12 @@ class DataRecordManager:
 
         print("Writing: Robot transform...")
         for i in tqdm.tqdm(range(self.robotNum), ncols=150):
+            npTime = np.array(self.dictTime)
             npDuration = np.array(self.dictDurationTime)
             npRobotPosition = np.array(self.dictRobotPosition["robot" + str(i + 1)])
             npRobotRotation = np.array(self.dictRobotRotation["robot" + str(i + 1)])
             npRobotTransform = np.concatenate([npRobotPosition, npRobotRotation], axis=1)
-            npTimeRobotTransform = np.c_[npDuration, npRobotTransform]
+            npTimeRobotTransform = np.c_[npTime, npDuration, npRobotTransform]
             self.ExportAsCSV(npTimeRobotTransform, dirPath, "Transform_Robot_" + str(i + 1), participant, conditions, number, robotHeader)
 
         # print("Writing: Robot Gripper value...")
