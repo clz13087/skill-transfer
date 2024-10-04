992b0a6a8952ed29b4c63436ecbe205023bcb508
clz13087 <clz13087@nitech.jp>
2024-10-04 15:44:27 +0900

    fix

--- SkillTransfer/Robot/CAMotion.py
+++ SkillTransfer/Robot/CAMotion.py
@@ -275,9 +275,9 @@ class CAMotion:
             weightedRot = np.dot(neomat4x4, self.weightedRotations["participant" + str(i + 1)])
 
             if i % 2 == 0:
-                sharedRotation_quaternion_left = (R.from_quat(weightedRot) * R.from_quat(sharedRotation_quaternion_left)).as_quat()
+                sharedRotation_quaternion_left = (self.quaternion_to_rotation_matrix(weightedRot) * self.quaternion_to_rotation_matrix(sharedRotation_quaternion_left)).as_quat()
             elif i % 2 == 1:
-                sharedRotation_quaternion_right = (R.from_quat(weightedRot) * R.from_quat(sharedRotation_quaternion_right)).as_quat()
+                sharedRotation_quaternion_right = (self.quaternion_to_rotation_matrix(weightedRot) * self.quaternion_to_rotation_matrix(sharedRotation_quaternion_right)).as_quat()
 
             self.weightedRotations["participant" + str(i + 1)] = weightedRot
             self.beforeRotations["participant" + str(i + 1)] = rotation["participant" + str(i + 1)]
