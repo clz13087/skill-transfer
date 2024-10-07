dc0d119b8a46b83d26b926c6767f9522c00e6b73
clz13087 <clz13087@nitech.jp>
2024-10-07 14:09:35 +0900

    fix

--- SkillTransfer/Recorder/DataRecord.py
+++ SkillTransfer/Recorder/DataRecord.py
@@ -152,11 +152,6 @@ class DataRecordManager:
             npTimeRigidBodyTransform = np.c_[npTime, npDuration, npRigidBodyTransform]
             self.ExportAsCSV(npTimeRigidBodyTransform, dirPath, "OtherRigidBody_" + str(i + 1), transformHeader)
 
-        # print("Writing: Participant Gripper value...")
-        # for i in tqdm.tqdm(range(self.bendingSensorNum), ncols=150):
-        #     npGripperValue_P = np.array(self.dictGripperValue_P["gripperValue_P" + str(i + 1)])
-        #     self.ExportAsCSV(npGripperValue_P, dirPath, "GripperValue_Participant_" + str(i + 1), participant, conditions, number, GripperHeader)
-
         print("Writing: Robot transform...")
         for i in tqdm.tqdm(range(self.robotNum), ncols=150):
             npTime = np.array(self.dictTime)
@@ -166,18 +161,6 @@ class DataRecordManager:
             npRobotTransform = np.concatenate([npRobotPosition, npRobotRotation], axis=1)
             npTimeRobotTransform = np.c_[npTime, npDuration, npRobotTransform]
             self.ExportAsCSV(npTimeRobotTransform, dirPath, "Transform_Robot_" + str(i + 1), robotHeader)
-
-        # print("Writing: Robot Gripper value...")
-        # for i in tqdm.tqdm(range(self.robotNum), ncols=150):
-        #     npGripperValue_R = np.array( self.dictGripperValue_R["gripperValue_R" + str(i + 1)])
-        #     self.ExportAsCSV( npGripperValue_R, dirPath, "GripperValue_Robot_" + str(i + 1), participant, conditions, number, GripperHeader)
-
-        # print("Writing: Head value...")
-        # npDuration = np.array(self.dictDurationTime)
-        # npHead = np.array(self.dictRobotHead)
-        # npHeadOutput = np.concatenate([npDuration, npHead], axis=1)
-        # self.ExportAsCSV(npHeadOutput, dirPath, "Head_", participant, conditions, number, headHeader)
-        # print("---------- Export completed ----------\n")
 
     def ExportAsCSV(self, data, dirPath, fileName, header: list = []):
         """
@@ -217,90 +200,3 @@ class DataRecordManager:
 
         if not os.path.isdir(path):
             os.makedirs(path)
-
-    def Quaternion2Euler(self, q, isDeg: bool = True):
-        """
-        Calculate the Euler angle from the Quaternion.
-
-
-        Rotation matrix
-        |m00 m01 m02 0|
-        |m10 m11 m12 0|
-        |m20 m21 m22 0|
-        | 0   0   0  1|
-
-        Parameters
-        ----------
-        q: np.ndarray
-            Quaternion.
-            [x, y, z, w]
-        isDeg: (Optional) bool
-            Returned angles are in degrees if this flag is True, else they are in radians.
-            The default is True.
-
-        Returns
-        ----------
-        rotEuler: np.ndarray
-            Euler angle.
-            [x, y, z]
-        """
-
-        qx = q[0]
-        qy = q[1]
-        qz = q[2]
-        qw = q[3]
-
-        # 1 - 2y^2 - 2z^2
-        m00 = 1 - (2 * qy**2) - (2 * qz**2)
-        # 2xy + 2wz
-        m01 = (2 * qx * qy) + (2 * qw * qz)
-        # 2xz - 2wy
-        m02 = (2 * qx * qz) - (2 * qw * qy)
-        # 2xy - 2wz
-        m10 = (2 * qx * qy) - (2 * qw * qz)
-        # 1 - 2x^2 - 2z^2
-        m11 = 1 - (2 * qx**2) - (2 * qz**2)
-        # 2yz + 2wx
-        m12 = (2 * qy * qz) + (2 * qw * qx)
-        # 2xz + 2wy
-        m20 = (2 * qx * qz) + (2 * qw * qy)
-        # 2yz - 2wx
-        m21 = (2 * qy * qz) - (2 * qw * qx)
-        # 1 - 2x^2 - 2y^2
-        m22 = 1 - (2 * qx**2) - (2 * qy**2)
-
-        # 回転軸の順番がX->Y->Zの固定角(Rz*Ry*Rx)
-        # if m01 == -1:
-        # 	tx = 0
-        # 	ty = math.pi/2
-        # 	tz = math.atan2(m20, m10)
-        # elif m20 == 1:
-        # 	tx = 0
-        # 	ty = -math.pi/2
-        # 	tz = math.atan2(m20, m10)
-        # else:
-        # 	tx = -math.atan2(m02, m00)
-        # 	ty = -math.asin(-m01)
-        # 	tz = -math.atan2(m21, m11)
-
-        # 回転軸の順番がX->Y->Zのオイラー角(Rx*Ry*Rz)
-        if m02 == 1:
-            tx = math.atan2(m10, m11)
-            ty = math.pi / 2
-            tz = 0
-        elif m02 == -1:
-            tx = math.atan2(m21, m20)
-            ty = -math.pi / 2
-            tz = 0
-        else:
-            tx = -math.atan2(-m12, m22)
-            ty = -math.asin(m02)
-            tz = -math.atan2(-m01, m00)
-
-        if isDeg:
-            tx = np.rad2deg(tx)
-            ty = np.rad2deg(ty)
-            tz = np.rad2deg(tz)
-
-        rotEuler = np.array([tx, ty, tz])
-        return rotEuler
